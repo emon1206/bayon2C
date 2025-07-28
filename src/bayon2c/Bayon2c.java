3bb8bbdf85895271ec58b5f4ca20da3d0fdd2ebf
sardidojoseph7 <sardidojoseph7@gmail.com>
2025-07-28 16:43:17 +0800

    dara akoa

---
+++ src/bayon2c/Bayon2c.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bayon2c;
+
+/**
+ *
+ * @author SCC_PC06
+ */
+public class Bayon2c {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
